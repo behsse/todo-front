c09152720134b0a4d0e8938001beaa4654a61126
Behsse <behsse.pro@gmail.com>
2023-04-20 00:03:43 +0200

    fix change api adress

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     async function getTodoList() {
       try {
-        const response = await fetch("https://todo-api-214z.onrender.com/getTodos");
+        const response = await fetch("https://todo-api-olw4.onrender.com/getTodos");
         if (response.ok) {
           const todos = await response.json();
           setTodoList(todos);
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -13,7 +13,7 @@ export default function AddTodo({ addTodo }) {
   async function addTodoToDatabase() {
     try {
       setLoading(true);
-      const response = await fetch("https://todo-api-214z.onrender.com/addTodo", {
+      const response = await fetch("https://todo-api-olw4.onrender.com/addTodo", {
         method: "POST",
         body: JSON.stringify({
           content: value,
